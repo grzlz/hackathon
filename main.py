13d7afc196933be228a7df355d5dcef22388a78c
grzlz <guillermo.rdzlpz@gmail.com>
2023-06-29 12:47:19 -0600

    create engine

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
-from funciones.saludo import saludo
+# from funciones.saludo import saludo
 
-saludo()
+# saludo()
+
+import openai
+
+# Replace with your own API key
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+def begin_conversation():
+    # response = openai.Completion.create(
+    #   engine="text-davinci-003",
+    #   prompt="Hello, how can I help you today?",
+    #   temperature=0.5,
+    #   max_tokens=100
+    # )
+    return "Yeyeye bienvenido"
+
+def continue_conversation(user_input):
+    response = get_gpt_response(user_input)
+    return response
+
+def get_gpt_response(prompt):
+    # response = openai.Completion.create(
+    #   engine="text-davinci-003",
+    #   prompt=prompt,
+    #   temperature=0.5,
+    #   max_tokens=100
+    # )
+    return "Respuesta de GPT"
+
+def main():
+    print(begin_conversation())
+    while True:
+        user_input = input(">> ")
+        if user_input == "Adios":
+            break
+        else:
+            print(continue_conversation(user_input))
+
+if __name__ == "__main__":
+    main()
