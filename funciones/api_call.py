971082525bc00b6679a5f1b2e177c113a6fdc615
AndrésP <andukial@gmail.com>
2023-06-29 23:47:15 -0600

    Integrar la api call a gpt al script de funciones.

--- funciones/api_call.py
+++ funciones/api_call.py
@@ -1,1 +1,10 @@
 # En este archivo estará todo lo relevante para hacer el API call a GPT3
+
+openai.api_key = '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'
+
+def get_gpt_response(prompt):
+    response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt = prompt ,
+    max_tokens=60)
+    return(response.choices[0].text.strip())
