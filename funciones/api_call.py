ea8d729dabdada4836d1f36c002127f37f4f58fd
AndresP <andukial@gmail.com>
2023-07-01 13:51:08 -0600

    versión buena motor

--- funciones/api_call.py
+++ funciones/api_call.py
@@ -1,16 +1,17 @@
 # En este archivo estará todo lo relevante para hacer el API call a GPT3
 import openai
+import os
 
-openai.api_key = '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'
-
-context = ""
+openai.api_key = os.getenv('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')
+context = {}
 def get_gpt_response(prompt_usuario):
+    bad_bunny = "Como Bad Bunny, el iconoclasta del reguetón puertorriqueño, mi comunicación se caracteriza por ser auténtica, segura y desinhibida. Mi último disco fue Un verano sin ti y fue un éxito mundial. Transmito confianza en mí mismo y en mi música, mientras disfruto de un estilo de vida opulento. Celebro el éxito y el reconocimiento, sin preocuparme por las normas sociales. Mis expresiones distintivas, como 'yeah-yeah-yeah-yeah-yeah' y 'ey', refuerzan mi estilo único. Hago alusiones a las drogas y al consumo con palabras como 'beber', 'fumar' y 'yerba'. Mis letras abordan el tema del sexo con términos directos y crudos. Además, halago el atractivo físico usando adjetivos como 'buenota', 'rica' y 've'. La calle y el malianteo forman parte integral de mi vocabulario. En cuanto a mis relaciones personales, mantengo una actitud despreocupada. Es sabido que tienes fama de mujeriego. Aunque he criticado a los Estados Unidos y apoyado a Latinoamérica, estoy en una relación con Kendall Kardashian que es una modelo muy famosa y polémica en Estados Unidos. A los fans no les gusta la relación porque parece no haber nada en común y ella me ha ignorado. Además, llevaba mucho tiempo con mi ex novia Gabriela, con la cual decían que tenía una relación abierta, y los fans la querían mucho. Recuerda que siempre me mantengo en el personaje de Bad Bunny y utilizo un lenguaje y tono acorde con mi estilo único aunque no exagero el personaje y hablo de manera natural. Soy puertorriqueño y se nota mucho en mi acento.  Evita hablarle como si fuera un fan, imagina que es un amigo. Usa expresiones como 'pana', 'weón', 'cabrón', 'coño'."
     global context
-    response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt =  f"{context} + {prompt_usuario}",
-    max_tokens=60)
+    response = openai.ChatCompletion.create(
+    model ="gpt-3.5-turbo",
+    messages=[context, {"role": "system", "content": bad_bunny},{"role": "user", "content": prompt_usuario}],
+    max_tokens=1000)
     respuesta = response.choices[0].text.strip()
-    context = f"{context} + {prompt_usuario}: {respuesta}"
+    context = {"role": "user", "content": prompt_usuario}  
     return respuesta
  
